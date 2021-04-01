08fdaf1e89bf357c22c403cceb21c31580d8f2a6
Guto Xavier <gutoffline@gmail.com>
2021-03-31 21:56:23 -0300

    calculo

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,6 +4,16 @@ let operacao = parseInt(prompt("Digite 1 para soma, 2 para subtração, 3 para m
 
 let segundoNumero = parseFloat(prompt("Informe o segundo número"));
 
-let resultado = primeiroNumero + segundoNumero;
-
-document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
+if(operacao == 1){
+    let resultado = primeiroNumero + segundoNumero;
+    document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 2){
+    let resultado = primeiroNumero - segundoNumero;
+    document.write("<h2>" + primeiroNumero + " - " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 3){
+    let resultado = primeiroNumero * segundoNumero;
+    document.write("<h2>" + primeiroNumero + " x " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 4){
+    let resultado = primeiroNumero / segundoNumero;
+    document.write("<h2>" + primeiroNumero + " / " + segundoNumero + " = " + resultado + "</h2>");
+}
